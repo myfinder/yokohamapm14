36b7e2887914c6ef592c49e8b643c9f3ebd2b5f2
Ubuntu <myfinder@mfdevelop01.3mftm0abqxdunbdmvwgm5jazxh.lx.internal.cloudapp.net>
2016-03-18 07:46:03 +0000

    this is initial commit

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+import {
+    "fmt"
+    "net/http"
+    "os"
+    "runtime"
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello! yokohama.go " + runtime.Version())
+}
+
+func main() {
+    http.HandleFunc("/", viewHandler)
+    http.ListenAndServe(":" + os.Getenv("HTTP_PLATFORM_PORT"), nil)
+}
