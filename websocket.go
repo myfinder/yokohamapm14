b78c83b8c9b4bdc9c4e178311afca4c792e88702
Tatsuro Hisamori <medianetworks@gmail.com>
2016-03-18 17:56:41 +0900

    oops3

--- websocket.go
+++ websocket.go
@@ -6,12 +6,14 @@ import (
     "net/http"
 )
 
-func echoHandler(ws *websocket.Conn) {
+func EchoHandler(ws *websocket.Conn) {
     io.Copy(ws, ws)
 }
 
 func main() {
-
-    http.Handle("/", websocket.Handler(echoHandler))
-    http.ListenAndServe(":" + os.Getenv("HTTP_PLATFORM_PORT"), nil)
+    http.Handle("/echo", websocket.Handler(EchoHandler))
+    err := http.ListenAndServe(":" + os.Getenv("HTTP_PLATFORM_PORT"), nil)
+    if err != nil {
+        panic("ListenAndServe: " + err.Error())
+    }
 }
