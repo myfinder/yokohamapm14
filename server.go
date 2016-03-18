f709264e0bea86e3576fe3c24045463ca458b16e
Tatsuro Hisamori <medianetworks@gmail.com>
2016-03-18 17:12:14 +0900

    Update server.go

--- server.go
+++ server.go
@@ -1,16 +1,17 @@
 package main
-import {
-    "fmt"
-    "net/http"
-    "os"
-    "runtime"
-}
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+  "runtime"
+)
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello! yokohama.go " + runtime.Version())
+  fmt.Fprintf(w, "Hello, yokohama.go " + runtime.Version())
 }
 
 func main() {
-    http.HandleFunc("/", viewHandler)
-    http.ListenAndServe(":" + os.Getenv("HTTP_PLATFORM_PORT"), nil)
+  http.HandleFunc("/", viewHandler)
+  http.ListenAndServe(":" + os.Getenv("HTTP_PLATFORM_PORT"), nil)
 }
